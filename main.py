a30a2ca769c2296f5bcef797c286b99db9984111
smpas <smpashts@gmail.com>
2023-11-08 22:48:51 +0300

    test commit

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import numpy as np
 from CityGeoTools.metrics.data import CityInformationModel as BaseModel
 
 city_model = BaseModel.CityInformationModel(city_name="Perm", city_crs=32640, cwd="CityGeoTools")
-print(city_model.city_name)
+print(city_model.city_crs)
