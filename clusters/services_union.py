a9ae64d7b15211683306dfafa39fbc79b56a4a1b
smpas <smpashts@gmail.com>
2023-11-23 21:54:47 +0300

    load buildings, services, blocks

---
+++ clusters/services_union.py
@@ -0,0 +1,24 @@
+import geopandas as gpd
+import os
+
+folder_path = 'C:/Users/sm_pa/OneDrive/Рабочий стол/analysys/citymodels/permModel/data/services'
+
+# Создаем пустой GeoDataFrame, который будет содержать все сервисы
+combined_gdf = gpd.GeoDataFrame()
+
+# Итерируемся по всем файлам в папке
+for filename in os.listdir(folder_path):
+    if filename.endswith(".geojson"):
+        # Загружаем GeoDataFrame из файла
+        service_gdf = gpd.read_file(os.path.join(folder_path, filename))
+
+        # Добавляем поле "service_code" с названием сервиса
+        service_gdf['service_code'] = filename[:-8]  # удаляем расширение ".geojson"
+
+        columns_to_keep = ['service_code', 'geometry']
+        service_gdf = service_gdf[columns_to_keep]
+        combined_gdf = combined_gdf.append(service_gdf, ignore_index=True)
+
+combined_gdf['id'] = range(1, len(combined_gdf) + 1)
+# Сохраняем объединенный GeoDataFrame в новый GeoJSON файл
+combined_gdf.to_file('C:/Users/sm_pa/OneDrive/Рабочий стол/analysys/citymodels/permModel/data/services.geojson', driver='GeoJSON')
--- main.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-import numpy as np
-from CityGeoTools.metrics.data import CityInformationModel as BaseModel
-
-city_model = BaseModel.CityInformationModel(city_name="Perm", city_crs=32640, cwd="CityGeoTools")
-print(city_model.city_name)
