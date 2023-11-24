c1c34bc25c9010431d949acb6729092f4a2dcee7
smpas <smpashts@gmail.com>
2023-11-24 18:05:10 +0300

    blocks clustering

--- clusters/block_clusters.py
+++ clusters/block_clusters.py
@@ -1,4 +1,3 @@
-import sys
 import folium
 import geopandas as gpd
 from CityGeoTools.metrics.data import CityInformationModel as BaseModel
@@ -6,7 +5,6 @@ from CityGeoTools.metrics.data import CityInformationModel as BaseModel
 city_model = BaseModel.CityInformationModel(city_name="Perm", city_crs=32640, cwd="CityGeoTools")
 
 city_model.update_layer("Buildings", "./data/buildings.geojson")
-city_model.update_layer("Services", "./data/services.geojson")
 city_model.update_layer("Blocks", "./data/blocks.geojson")
 
 from CityGeoTools.metrics.calculations import spacematrix
@@ -20,12 +18,12 @@ def style_fn(feature):
     ]
     return {'fillColor': colors[int(feature["properties"]["spacematrix_cluster"])]}
 
-blocks_morphotypes_61 = blocks_morphotypes[blocks_morphotypes["administrative_unit_id"] == 61]
-blocks_morphotypes_61 = blocks_morphotypes_61.dropna(subset=["spacematrix_cluster"])
-map = folium.Map(location = [59.931, 30.250], zoom_start = 13, tiles='CartoDB positron')
+blocks_morphotypes_2 = blocks_morphotypes[blocks_morphotypes["administrative_unit_id"] == 2]
+blocks_morphotypes_2 = blocks_morphotypes_2.dropna(subset=["spacematrix_cluster"])
+map = folium.Map(location = [57.944826, 56.158986], zoom_start = 13, tiles='CartoDB positron')
 geo_j = folium.GeoJson(
-    data=blocks_morphotypes_61,
+    data=blocks_morphotypes_2,
     style_function=style_fn,
     tooltip=folium.features.GeoJsonTooltip(["FSI", "GSI", "L", "MXI", "OSR", "spacematrix_cluster"])).add_to(map)
 geo_j.add_to(map)
-map
+map.save("clusters/block_clusters.html")
